45a30927cc33f1766ac3ecd061721ab9ee86b91a
Justin Desrochers <justin.desrochers@meltwater.com>
2014-07-25 08:47:42 -0400

    Initial version of the chatroom service that contains functions to create a message and get recent messages

--- chatroom/public/js/services/chatroom_service.js
+++ chatroom/public/js/services/chatroom_service.js
@@ -1,3 +1,19 @@
 'use strict';
-chatroomApp.service('ChatroomService', ['$http', function($http) {
+chatroomApp.service('ChatroomService', ['$http', 'CoreUserService', function($http, CoreUserService) {
+    var campfireService = {},
+        appSettings = CoreUserService.getAppSettings('chatroom'),
+        campfireAPIToken = '86f0e7a972be8a09a31c0346e87493dce3b91a40',
+        CAMPFIRE_DOMAIN = "https://" + campfireAPIToken + ":x@meltwater.campfirenow.com";
+
+    campfireService.getRecentMessages = function getRecentMessages(roomId) {
+        var url = CAMPFIRE_DOMAIN + "/room/" + roomId + "/recent.json";
+        return $http.get(url);
+    };
+
+    campfireService.createMessage = function createMessage(roomId, message) {
+        var url = CAMPFIRE_DOMAIN + "/room/" + roomId + "/speak.json";
+        return $http.post(url, message);
+    };
+
+    return campfireService;
 }]);
